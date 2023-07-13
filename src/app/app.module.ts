2b974a83443e49d905b943cb98c4a6a0eab0ed8b
weronikapanek <werkap8811@gmail.com>
2023-07-13 07:05:05 +0200

    changing double quotes to single in imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import {NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import {PlayingAreaComponent} from "../components/playing-area/playing-area.component";
-import {HeaderComponent} from "../components/header/header.component";
-import {HttpClientModule} from "@angular/common/http";
+import {PlayingAreaComponent} from '../components/playing-area/playing-area.component';
+import {HeaderComponent} from '../components/header/header.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
